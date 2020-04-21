9411218a482eecc62e50548a56884f90f0366948
Batura Andrey <batura.andrey@otr.ru>
2020-04-21 21:44:22 +0300

    feat: contacts in footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -4,7 +4,29 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <div id="footer" className="footer-section">
-      Footer
+      <section className="contacts">
+        <div className="contact-container flex-row">
+          <div className="contact__block">
+            <div className="github">
+              <a href="https://github.com/BaturaAndrew">
+                for contact Batura Andrew
+              </a>
+            </div>
+            <div className="gmail">
+              <a href="mailto:baturaandrew@gmail.com">baturaandrew@gmail.com</a>
+            </div>
+          </div>
+
+          <div className="contact__block">
+            <div className="skype">
+              <a href="skype: batura_andrew">skype: batura_andrew</a>
+            </div>
+            <div className="phone">
+              mob: <em>+37529 263 72 22</em>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
--- src/component/Header/Hello/Hello.js
+++ src/component/Header/Hello/Hello.js
@@ -7,18 +7,17 @@ const Hello = () => {
   return (
     <div id="hello" className="header-section flex-column">
       <div className="profile-detail flex-column">
-        <span className="name"> Hello, I'm Batura Andrew.</span> <br />
+        <span className="name"> Hello, I'm Batura Andrew.</span>
         <p className="hello">
-          I build responsive websites, easy to use JS libraries. Look below at
-          my&nbsp;
-          <Link to="projects" smooth={true} offset={-80} duration={500}>
+          I build responsive websites. Look below at my&nbsp;
+          <Link to="projects" smooth={true} offset={-80}>
             works
           </Link>
           , and&nbsp;
-          <Link to="contacts" smooth={true} offset={-80} duration={500}>
+          <Link to="contacts" smooth={true} offset={-80}>
             contact
-          </Link>{' '}
-          me. Feel free
+          </Link>
+          me. Feel free.
         </p>
       </div>
       <Button
