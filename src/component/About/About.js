bf5d49495eb066ec08b1cbae89a6b0c3d15d815e
Batura Andrey <batura.andrey@otr.ru>
2020-04-22 21:38:00 +0300

    fix: about text

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -11,19 +11,27 @@ const About = () => {
         </Divider>
         <div className="about-me flex-row">
           <img className="about-me__ava" src={ava} alt="avatar" />
-          <div
-            className="about-me__text"
-            data-aos="zoom-out-left"
-            data-aos-duration="500">
-            I am working at the united institute of informatic problems, in the
-            laboratory of automation of technological processes. I develop
-            software for automatic creating sketches of technological
-            operations. Currenly I am working for Organizational and
-            Technological Solutions as software engineer. I also studied
-            independently node.js, an example in the gallery-task
-            <h3>Education</h3>I graduated from BNTU with a specialty in
-            automated design system faculty of information technologies and
-            robotics I finished ABAP courses at LeverX in 2015.
+          <div className="flex-column">
+            <div
+              className="about-me__text"
+              data-aos="zoom-out-left"
+              data-aos-duration="500">
+              <h3>education</h3>I graduated from BNTU with a specialty in
+              automated design system faculty of information technologies and
+              robotics.
+            </div>
+            <div
+              className="about-me__text"
+              data-aos="zoom-out-left"
+              data-aos-duration="500">
+              <h3>work</h3> I worked at the United institute of informatic
+              problems, in the laboratory of automation of technological
+              processes. I develop software for automatic creating sketches of
+              technological operations. Currenly I am working at Organizational
+              and Technological Solutions as software engineer. The job is
+              related to very old IBM RPG programming languige, so I decided to
+              explore some new programming area.
+            </div>
           </div>
         </div>
       </div>
